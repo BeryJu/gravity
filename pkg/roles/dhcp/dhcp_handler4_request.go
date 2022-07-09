981bbc761077498e78331a40ec854015c3aca891
Jens Langhammer <jens@beryju.org>
2022-07-09 15:39:53 +0000

    update

--- pkg/roles/dhcp/dhcp_handler4_request.go
+++ pkg/roles/dhcp/dhcp_handler4_request.go
@@ -18,10 +18,9 @@ func (r *DHCPRole) handleDHCPRequest4(conn net.PacketConn, peer net.Addr, m *dhc
 		r.log.Debug("found scope for new lease")
 		match = scope.createLeaseFor(conn, peer, m)
 		r.log.Debug("creating new lease")
-		match.put(scope.TTL)
 	}
 
-	// TODO: Update
+	match.put(match.scope.TTL)
 	match.reply(conn, peer, m, func(d *dhcpv4.DHCPv4) *dhcpv4.DHCPv4 {
 		d.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeAck))
 		return d
@@ -30,26 +29,4 @@ func (r *DHCPRole) handleDHCPRequest4(conn net.PacketConn, peer net.Addr, m *dhc
 
 func (r *DHCPRole) findLease(m *dhcpv4.DHCPv4) *Lease {
 	return nil
-}
-
-func (r *DHCPRole) createLease(lease *Lease, scope *Scope) {
-	// err := r.Create(context.Background(), lease)
-	// if err != nil {
-	// 	r.l.Error(err, "failed to create lease")
-	// }
-	// r.i.DispatchEvent(roles.NewEvent[any](
-	// 	[]string{KeyRole, KeyLeases, "create"},
-	// 	// Leas
-	// ))
-
-	// TODO: Send DNS create event
-	// dns, err := dns.GetDNSProviderForScope(*scope)
-	// if err != nil {
-	// 	r.l.Error(err, "failed to get DNS provider")
-	// 	return
-	// }
-	// err = dns.CreateRecord(lease)
-	// if err != nil {
-	// 	r.l.Error(err, "failed to delete DNS record")
-	// }
 }
