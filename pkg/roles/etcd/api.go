0be7f9c417fe5b64fc493fe915ca1d947a241d1d
Jens Langhammer <jens.langhammer@beryju.org>
2022-07-10 14:33:11 +0200

    add join

--- pkg/roles/etcd/api.go
+++ pkg/roles/etcd/api.go
@@ -2,8 +2,10 @@ package etcd
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"beryju.io/ddet/pkg/extconfig"
 	"beryju.io/ddet/pkg/roles"
 	"github.com/go-chi/chi/v5"
 )
@@ -17,5 +19,18 @@ func (ro *EmbeddedEtcd) eventHandlerAPIMux(ev *roles.Event) {
 			return
 		}
 		json.NewEncoder(w).Encode(members.Members)
+	})
+	mux.Post("/v0/etcd/join", func(w http.ResponseWriter, r *http.Request) {
+		_, err := ro.i.KV().MemberAddAsLearner(r.Context(), []string{r.URL.Query().Get("peer")})
+		if err != nil {
+			ro.log.WithError(err).Warning("added member")
+			return
+		}
+		env := fmt.Sprintf(
+			"ETCD_JOIN_CLUSTER='%s,%s'",
+			extconfig.Get().Instance.Identifier,
+			extconfig.Get().Instance.IP,
+		)
+		w.Write([]byte(env))
 	})
 }
