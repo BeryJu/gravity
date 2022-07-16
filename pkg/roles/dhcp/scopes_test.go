65b6d03ce4f54e2ec65a11f011176e68fbf6976f
Jens Langhammer <jens.langhammer@beryju.org>
2022-07-16 12:07:19 +0200

    fix build errors

--- pkg/roles/dhcp/scopes_test.go
+++ pkg/roles/dhcp/scopes_test.go
@@ -5,6 +5,7 @@ import (
 	"net/netip"
 	"testing"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -12,6 +13,9 @@ func TestMatch(t *testing.T) {
 	s := Scope{
 		cidr: netip.MustParsePrefix("192.0.2.0/24"),
 	}
-	assert.Equal(t, s.match(net.ParseIP("192.0.2.2"), nil), 24)
-	assert.Equal(t, s.match(net.ParseIP("192.3.2.2"), nil), -1)
+	req := &Request{
+		log: log.WithField("foo", "bar"),
+	}
+	assert.Equal(t, s.match(net.ParseIP("192.0.2.2"), req), 24)
+	assert.Equal(t, s.match(net.ParseIP("192.3.2.2"), req), -1)
 }
