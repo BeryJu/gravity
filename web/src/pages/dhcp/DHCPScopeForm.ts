ab34a14b79644aa6537c23d897065e538365b01c
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-20 18:28:38 +0200

    more ui

---
+++ web/src/pages/dhcp/DHCPScopeForm.ts
@@ -0,0 +1,68 @@
+import YAML from "yaml";
+
+import { TemplateResult, html } from "lit";
+import { customElement } from "lit/decorators.js";
+import { ifDefined } from "lit/directives/if-defined.js";
+
+import { DhcpScope, RolesDhcpApi } from "@beryju/gravity-api";
+
+import { DEFAULT_CONFIG } from "../../api/Config";
+import "../../elements/CodeMirror";
+import "../../elements/forms/HorizontalFormElement";
+import { ModelForm } from "../../elements/forms/ModelForm";
+
+@customElement("gravity-dhcp-zone-form")
+export class DHCPScopeForm extends ModelForm<DhcpScope, string> {
+    loadInstance(pk: string): Promise<DhcpScope> {
+        return new RolesDhcpApi(DEFAULT_CONFIG).dhcpGetScopes().then((scopes) => {
+            const zone = scopes.scopes?.find((z) => z.scope === pk);
+            if (!zone) throw new Error("No scope");
+            return zone;
+        });
+    }
+
+    getSuccessMessage(): string {
+        if (this.instance) {
+            return `Successfully updated scope.`;
+        } else {
+            return `Successfully created scope.`;
+        }
+    }
+
+    send = (data: DhcpScope): Promise<void> => {
+        return new RolesDhcpApi(DEFAULT_CONFIG).dhcpPutScopes({
+            scope: data.scope,
+            dhcpScopesInput: data,
+        });
+    };
+
+    renderForm(): TemplateResult {
+        return html`<form class="pf-c-form pf-m-horizontal">
+            ${this.instance ? html`` : html`<ak-form-element-horizontal label="Name" ?required=${true} name="scope">
+                <input
+                    type="text"
+                    class="pf-c-form-control"
+                    required
+                />
+            </ak-form-element-horizontal>`}
+            <ak-form-element-horizontal name="_default">
+                <div class="pf-c-check">
+                    <input
+                        type="checkbox"
+                        class="pf-c-check__input"
+                        ?checked=${this.instance?._default}
+                    />
+                    <label class="pf-c-check__label"> ${`Default`} </label>
+                </div>
+            </ak-form-element-horizontal>
+            <ak-form-element-horizontal label="TTL" ?required=${true} name="ttl">
+                <input
+                    type="number"
+                    value="${this.instance?.ttl || 86400}"
+                    class="pf-c-form-control"
+                    required
+                />
+            </ak-form-element-horizontal>
+        </form>`;
+    }
+}
--- web/src/pages/dhcp/DHCPScopesPage.ts
+++ web/src/pages/dhcp/DHCPScopesPage.ts
@@ -1,12 +1,19 @@
 import { TemplateResult, html } from "lit";
 import { customElement } from "lit/decorators.js";
 
+
+
 import { DhcpScope, RolesDhcpApi } from "@beryju/gravity-api";
 
+
+
 import { DEFAULT_CONFIG } from "../../api/Config";
+import "../../elements/forms/ModalForm";
 import { PaginatedResponse, TableColumn } from "../../elements/table/Table";
 import { TablePage } from "../../elements/table/TablePage";
 import { PaginationWrapper } from "../../utils";
+import "./DHCPScopeForm";
+
 
 @customElement("gravity-dhcp-scopes")
 export class DHCPScopesPage extends TablePage<DhcpScope> {
@@ -32,5 +39,15 @@ export class DHCPScopesPage extends TablePage<DhcpScope> {
             html`<a href=${`#/dhcp/scopes/${item.scope}`}>${item.scope}</a>`,
             html`${item.subnetCidr}`,
         ];
+    }
+    renderObjectCreate(): TemplateResult {
+        return html`
+            <ak-forms-modal>
+                <span slot="submit"> ${`Create`} </span>
+                <span slot="header"> ${`Create Scope`} </span>
+                <gravity-dhcp-zone-form slot="form"> </gravity-dhcp-zone-form>
+                <button slot="trigger" class="pf-c-button pf-m-primary">${`Create`}</button>
+            </ak-forms-modal>
+        `;
     }
 }
