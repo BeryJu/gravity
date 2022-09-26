5d3e989883dfc283c9e3c28a16ffcf3f32dea9ae
Jens Langhammer <jens.langhammer@beryju.org>
2022-09-26 23:29:10 +0200

    add dhcp leaseNegotiateTimeout

--- web/src/pages/cluster/RoleDHCPConfigForm.ts
+++ web/src/pages/cluster/RoleDHCPConfigForm.ts
@@ -24,7 +24,6 @@ export class RoleDHCPConfigForm extends ModelForm<DhcpRoleConfig, string> {
     }
 
     send = (data: DhcpRoleConfig): Promise<unknown> => {
-        console.log(data);
         return new RolesDhcpApi(DEFAULT_CONFIG).dhcpPutRoleConfig({
             dhcpRoleDHCPConfigInput: {
                 config: data,
@@ -41,6 +40,21 @@ export class RoleDHCPConfigForm extends ModelForm<DhcpRoleConfig, string> {
                     class="pf-c-form-control"
                     required
                 />
+            </ak-form-element-horizontal>
+            <ak-form-element-horizontal
+                label="Lease negotiation timeout"
+                ?required=${true}
+                name="leaseNegotiateTimeout"
+            >
+                <input
+                    type="number"
+                    value="${first(this.instance?.leaseNegotiateTimeout, 30)}"
+                    class="pf-c-form-control"
+                    required
+                />
+                <p class="pf-c-form__helper-text">
+                    Time in seconds a client has to acknowledge an IP after it has been offered.
+                </p>
             </ak-form-element-horizontal>
         </form>`;
     }
