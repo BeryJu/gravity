5b123b2a498244c19bee7a2982de3839ab924522
Jens Langhammer <jens.langhammer@beryju.org>
2022-12-27 18:58:36 +0100

    add SRV fields and labels

--- web/src/pages/dns/DNSRecordForm.ts
+++ web/src/pages/dns/DNSRecordForm.ts
@@ -1,7 +1,7 @@
 import { DnsAPIRecord, RolesDnsApi } from "gravity-api";
 
 import { TemplateResult, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 import { ifDefined } from "lit/directives/if-defined.js";
 
 import { DEFAULT_CONFIG } from "../../api/Config";
@@ -13,6 +13,9 @@ export class DNSRecordForm extends ModelForm<DnsAPIRecord, string> {
     @property()
     zone?: string;
 
+    @state()
+    recordType = "A";
+
     loadInstance(pk: string): Promise<DnsAPIRecord> {
         return new RolesDnsApi(DEFAULT_CONFIG)
             .dnsGetRecords({
@@ -21,6 +24,7 @@ export class DNSRecordForm extends ModelForm<DnsAPIRecord, string> {
             .then((records) => {
                 const record = records.records?.find((z) => z.hostname + z.uid === pk);
                 if (!record) throw new Error("No record");
+                this.recordType = record.type;
                 return record;
             });
     }
@@ -57,6 +61,19 @@ export class DNSRecordForm extends ModelForm<DnsAPIRecord, string> {
         });
     };
 
+    getLabel(): string {
+        switch (this.recordType) {
+            case "CNAME":
+                return "CNAME Target";
+            case "SRV":
+                return "SRV Target";
+            case "MX":
+                return "Mail server";
+            default:
+                return "Data";
+        }
+    }
+
     renderForm(): TemplateResult {
         return html`<form class="pf-c-form pf-m-horizontal">
             <ak-form-element-horizontal label="Hostname" ?required=${true} name="hostname">
@@ -79,16 +96,25 @@ export class DNSRecordForm extends ModelForm<DnsAPIRecord, string> {
                 </p>
             </ak-form-element-horizontal>
             <ak-form-element-horizontal label="Type" ?required=${true} name="type">
-                <select class="pf-c-form-control">
+                <select
+                    class="pf-c-form-control"
+                    @change=${(ev: Event) => {
+                        const current = (ev.target as HTMLInputElement).value;
+                        this.recordType = current;
+                    }}
+                >
                     <option value="A" ?selected=${this.instance?.type === "A"}>A</option>
                     <option value="AAAA" ?selected=${this.instance?.type === "AAAA"}>AAAA</option>
-                    <option value="CNAME" ?selected=${this.instance?.type === "CNAME"}>CNAME</option>
+                    <option value="CNAME" ?selected=${this.instance?.type === "CNAME"}>
+                        CNAME
+                    </option>
                     <option value="PTR" ?selected=${this.instance?.type === "PTR"}>PTR</option>
                     <option value="NS" ?selected=${this.instance?.type === "NS"}>NS</option>
                     <option value="MX" ?selected=${this.instance?.type === "MX"}>MX</option>
+                    <option value="SRV" ?selected=${this.instance?.type === "SRV"}>SRV</option>
                 </select>
             </ak-form-element-horizontal>
-            <ak-form-element-horizontal label="Data" ?required=${true} name="data">
+            <ak-form-element-horizontal label="${this.getLabel()}" ?required=${true} name="data">
                 <input
                     type="text"
                     value="${ifDefined(this.instance?.data)}"
@@ -96,6 +122,58 @@ export class DNSRecordForm extends ModelForm<DnsAPIRecord, string> {
                     required
                 />
             </ak-form-element-horizontal>
+            ${this.recordType === "MX"
+                ? html`
+                      <ak-form-element-horizontal
+                          label="MX Preference"
+                          ?required=${true}
+                          name="mxPreference"
+                      >
+                          <input
+                              type="number"
+                              value="${ifDefined(this.instance?.mxPreference)}"
+                              class="pf-c-form-control"
+                              required
+                          />
+                      </ak-form-element-horizontal>
+                  `
+                : html``}
+            ${this.recordType === "SRV"
+                ? html`
+                      <ak-form-element-horizontal label="SRV Port" ?required=${true} name="srvPort">
+                          <input
+                              type="number"
+                              value="${ifDefined(this.instance?.srvPort)}"
+                              class="pf-c-form-control"
+                              required
+                          />
+                      </ak-form-element-horizontal>
+                      <ak-form-element-horizontal
+                          label="SRV Priority"
+                          ?required=${true}
+                          name="srvPriority"
+                      >
+                          <input
+                              type="number"
+                              value="${ifDefined(this.instance?.srvPriority)}"
+                              class="pf-c-form-control"
+                              required
+                          />
+                      </ak-form-element-horizontal>
+                      <ak-form-element-horizontal
+                          label="SRV Weight"
+                          ?required=${true}
+                          name="srvWeight"
+                      >
+                          <input
+                              type="number"
+                              value="${ifDefined(this.instance?.srvWeight)}"
+                              class="pf-c-form-control"
+                              required
+                          />
+                      </ak-form-element-horizontal>
+                  `
+                : html``}
         </form>`;
     }
 }
