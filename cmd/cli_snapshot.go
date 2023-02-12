ee61ed03522b5359f3e699c6f8abcc28ac988daa
Jens Langhammer <jens.langhammer@beryju.org>
2023-02-12 22:04:00 +0100

    make etcd cli use table by default

--- cmd/cli_snapshot.go
+++ cmd/cli_snapshot.go
@@ -7,7 +7,7 @@ import (
 
 func init() {
 	sc := etcdutl.NewSnapshotCommand()
-	sc.PersistentFlags().StringVarP(&etcdutl.OutputFormat, "write-out", "w", "simple", "set the output format (fields, json, protobuf, simple, table)")
+	sc.PersistentFlags().StringVarP(&etcdutl.OutputFormat, "write-out", "w", "table", "set the output format (fields, json, protobuf, simple, table)")
 	sc.RegisterFlagCompletionFunc("write-out", func(_ *cobra.Command, _ []string, _ string) ([]string, cobra.ShellCompDirective) {
 		return []string{"fields", "json", "protobuf", "simple", "table"}, cobra.ShellCompDirectiveDefault
 	})
