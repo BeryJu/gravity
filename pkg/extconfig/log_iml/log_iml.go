0577c220ceed2981043045a4d6b62095f0d883cb
Jens Langhammer <jens.langhammer@beryju.org>
2023-06-18 18:43:07 +0200

    add locking to in-memory-logger

--- pkg/extconfig/log_iml/log_iml.go
+++ pkg/extconfig/log_iml/log_iml.go
@@ -2,25 +2,33 @@ package log_iml
 
 import (
 	"net/url"
+	"sync"
 
 	"go.uber.org/zap"
 )
 
 type inMemoryLogger struct {
 	msgs []string
+	msgM sync.RWMutex
 	max  int
 }
 
 func (iml *inMemoryLogger) Close() error { return nil }
 func (iml *inMemoryLogger) Sync() error  { return nil }
 func (iml *inMemoryLogger) Write(log []byte) (int, error) {
-	iml.msgs = append(iml.msgs, string(log))
-	if len(iml.msgs) > iml.max {
-		iml.msgs = iml.msgs[1:]
-	}
+	go func() {
+		iml.msgM.Lock()
+		iml.msgs = append(iml.msgs, string(log))
+		if len(iml.msgs) > iml.max {
+			iml.msgs = iml.msgs[1:]
+		}
+		iml.msgM.Unlock()
+	}()
 	return 0, nil
 }
 func (iml *inMemoryLogger) Messages() []string {
+	iml.msgM.RLock()
+	defer iml.msgM.RUnlock()
 	return iml.msgs
 }
 
