6f2bc2c1e8a77c3e5f6b4b1ffe2e544314ea29c5
Jens Langhammer <jens.langhammer@beryju.org>
2024-01-28 21:01:47 +0100

    fix build

--- web/rollup.config.js
+++ web/rollup.config.js
@@ -2,7 +2,7 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import { terser } from "@rollup/plugin-terser";
+import terser from "@rollup/plugin-terser";
 import copy from "rollup-plugin-copy";
 import cssimport from "rollup-plugin-cssimport";
 
