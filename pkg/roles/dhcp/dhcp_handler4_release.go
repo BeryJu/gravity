b75da6536243b36b90fbb159c0ceb54464959da1
Jens Langhammer <jens.langhammer@beryju.org>
2024-10-21 00:15:16 +0200

    fix

--- pkg/roles/dhcp/dhcp_handler4_release.go
+++ pkg/roles/dhcp/dhcp_handler4_release.go
@@ -7,26 +7,14 @@ import (
 
 func (r *Role) HandleDHCPRelease4(req *Request4) *dhcpv4.DHCPv4 {
 	match := r.FindLease(req)
-	if match == nil {
-		scope := r.findScopeForRequest(req)
-		if scope == nil {
-			req.log.Info("no scope found")
-			return nil
-		}
-		req.log.Debug("found scope for new lease", zap.String("scope", scope.Name))
-		match = scope.leaseFor(req)
-		if match == nil {
-			return nil
-		}
+	if match == nil || match.IsReservation() {
 		return nil
 	}
-	if !match.IsReservation() {
-		err := match.Delete(req.Context)
-		if err != nil {
-			req.log.Warn("failed to put lease", zap.Error(err))
-		} else {
-			req.log.Info("deleted lease from release")
-		}
+	err := match.Delete(req.Context)
+	if err != nil {
+		req.log.Warn("failed to delete lease", zap.Error(err))
+	} else {
+		req.log.Info("deleted lease from release")
 	}
 	dhcpRequests.WithLabelValues(req.MessageType().String(), match.scope.Name).Inc()
 	return nil
