caa880457ba8000af5b6a63f9cfb13231b76d8dd
Jens Langhammer <jens.langhammer@beryju.org>
2024-11-03 20:56:43 +0100

    fix blocky allowlists, add tests

--- pkg/roles/dns/handler_forward_blocky_test.go
+++ pkg/roles/dns/handler_forward_blocky_test.go
@@ -61,3 +61,53 @@ func TestRoleDNS_BlockyForwarder(t *testing.T) {
 	ans := fw.Msg().Answer[0]
 	assert.Equal(t, net.ParseIP("0.0.0.0").String(), ans.(*d.A).A.String())
 }
+
+func TestRoleDNS_BlockyForwarder_Allow(t *testing.T) {
+	defer tests.Setup(t)()
+	rootInst := instance.New()
+	ctx := tests.Context()
+	inst := rootInst.ForRole("dns", ctx)
+	tests.PanicIfError(inst.KV().Delete(
+		ctx,
+		inst.KV().Key(
+			types.KeyRole,
+			types.KeyZones,
+		).Prefix(true).String(),
+		clientv3.WithPrefix(),
+	))
+	tests.PanicIfError(inst.KV().Put(
+		ctx,
+		inst.KV().Key(
+			types.KeyRole,
+			types.KeyZones,
+			".",
+		).String(),
+		tests.MustJSON(dns.Zone{
+			HandlerConfigs: []map[string]string{
+				{
+					"type":       "forward_blocky",
+					"blocklists": "http://127.0.0.1:9005/blocky_file.txt",
+					"allowlists": "gravity.beryju.io",
+					"to":         "127.0.0.1:1053",
+				},
+			},
+		}),
+	))
+	role := dns.New(inst)
+	assert.NotNil(t, role)
+	assert.Nil(t, role.Start(ctx, RoleConfig()))
+	defer role.Stop()
+
+	fw := NewNullDNSWriter()
+	role.Handler(fw, &d.Msg{
+		Question: []d.Question{
+			{
+				Name:   "gravity.beryju.io.",
+				Qtype:  d.TypeA,
+				Qclass: d.ClassINET,
+			},
+		},
+	})
+	ans := fw.Msg().Answer[0]
+	assert.Equal(t, net.ParseIP("10.0.0.1").String(), ans.(*d.A).A.String())
+}
