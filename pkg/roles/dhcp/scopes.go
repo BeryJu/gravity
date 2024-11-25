6272435f187b04d2dab5820540e4c9ee4c966b6a
Jens L. <jens.langhammer@beryju.org>
2024-11-25 04:26:41 +0100

    dhcp: fix scope moving when it shouldn't (#1318)

--- pkg/roles/dhcp/scopes.go
+++ pkg/roles/dhcp/scopes.go
@@ -104,8 +104,16 @@ func (r *Role) findScopeForRequest(req *Request4) *Scope {
 	// To prioritise requests from a DHCP relay being matched correctly, give their subnet
 	// match a 1 bit more priority
 	const dhcpRelayBias = 1
+	const clientIPBias = 2
 	for _, scope := range r.scopes {
-		// Check based on gateway IP (highest priority)
+		// Check based on Client IP Address (highest priority)
+		clientIPMatchBits := scope.match(req.ClientIPAddr)
+		if clientIPMatchBits > -1 && clientIPMatchBits+clientIPBias > longestBits {
+			req.log.Debug("selected scope based on client IP", zap.String("scope", scope.Name))
+			match = scope
+			longestBits = clientIPMatchBits + clientIPBias
+		}
+		// Check based on gateway IP (next highest priority)
 		gatewayMatchBits := scope.match(req.GatewayIPAddr)
 		if gatewayMatchBits > -1 && gatewayMatchBits+dhcpRelayBias > longestBits {
 			req.log.Debug("selected scope based on cidr match (gateway IP)", zap.String("scope", scope.Name))
