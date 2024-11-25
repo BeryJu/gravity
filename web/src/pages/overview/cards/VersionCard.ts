d51fac2d553060df2178ad38372b5a8372dacc3a
Jens Langhammer <jens.langhammer@beryju.org>
2024-11-25 04:02:24 +0100

    fix some badly named API endpoints
    # Conflicts:
    #	pkg/instance/api_instance.go
    #	schema.yml
    #	web/src/pages/overview/cards/VersionCard.ts

--- web/src/pages/overview/cards/VersionCard.ts
+++ web/src/pages/overview/cards/VersionCard.ts
@@ -1,4 +1,7 @@
-import { ClusterInstancesApi, InstanceAPIInstanceInfo } from "gravity-api";
+import {
+    ClusterApi,
+    InstanceAPIClusterInfoOutput,
+} from "gravity-api";
 
 import { TemplateResult, html } from "lit";
 import { customElement } from "lit/decorators.js";
@@ -7,27 +10,37 @@ import { DEFAULT_CONFIG } from "../../../api/Config";
 import { AdminStatus, AdminStatusCard } from "./AdminStatusCard";
 
 @customElement("gravity-overview-card-version")
-export class VersionCard extends AdminStatusCard<InstanceAPIInstanceInfo> {
+export class VersionCard extends AdminStatusCard<InstanceAPIClusterInfoOutput> {
     header = "Version";
     headerLink = "#/cluster/nodes";
 
-    getPrimaryValue(): Promise<InstanceAPIInstanceInfo> {
-        return new ClusterInstancesApi(DEFAULT_CONFIG).clusterGetInfo();
+    async getPrimaryValue(): Promise<InstanceAPIClusterInfoOutput> {
+        return await new ClusterApi(DEFAULT_CONFIG).clusterGetClusterInfo();
     }
 
-    getStatus(value: InstanceAPIInstanceInfo): Promise<AdminStatus> {
+    getStatus(value: InstanceAPIClusterInfoOutput): Promise<AdminStatus> {
+        const matching =
+            value.instances?.filter((inst) => {
+                return inst.version === value.clusterVersion;
+            }).length === value.instances?.length;
+        if (!matching) {
+            return Promise.resolve<AdminStatus>({
+                icon: "fa fa-exclamation-triangle pf-m-warning",
+                message: html`Mismatched version in cluster!`,
+            });
+        }
         return Promise.resolve<AdminStatus>({
             icon: "fa fa-check-circle pf-m-success",
-            message: html`${value?.buildHash.substring(0, 7)}`,
+            message: html`Matching versions across nodes.`,
         });
     }
 
     renderValue(): TemplateResult {
         return html`<a
-            href="https://github.com/BeryJu/gravity/commit/${this.value?.buildHash}"
+            href="https://github.com/BeryJu/gravity/releases/tag/${this.value?.clusterVersionShort}"
             target="_blank"
         >
-            ${this.value?.version}
+            ${this.value?.clusterVersion}
         </a>`;
     }
 }
