ca731fdbd8c67414e3ce2e3e9d5afc0fd797dfab
Jens Langhammer <jens.langhammer@beryju.org>
2025-01-11 19:35:13 +0100

    more testing files

--- pkg/convert/bind/convert_test.go
+++ pkg/convert/bind/convert_test.go
@@ -11,6 +11,7 @@ import (
 	"beryju.io/gravity/pkg/tests"
 	"beryju.io/gravity/pkg/tests/api"
 	"github.com/stretchr/testify/assert"
+	clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 func TestBindImport(t *testing.T) {
@@ -69,6 +70,76 @@ func TestBindImport(t *testing.T) {
 				},
 			},
 		},
+		{
+			file: "./fixtures/example2.zone",
+			kv: []kv{
+				{
+					key: ri.KV().Key(types.KeyRole, types.KeyZones, "example.com."),
+					values: []interface{}{
+						dns.Zone{DefaultTTL: 604800, Authoritative: true, HandlerConfigs: []map[string]interface{}{
+							{
+								"type": "etcd",
+							},
+						}},
+					},
+				},
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", types.DNSRootRecord, "MX", "bind-import"),
+				// 	values: []interface{}{`{"data":"mail.example.net.","mxPreference":20}`},
+				// },
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "ftp.", "CNAME", "bind-import"),
+				// 	values: []interface{}{`{"data":"ftp.example.net."}`},
+				// },
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "joe.", "A", "bind-import"),
+				// 	values: []interface{}{`{"data":"192.168.254.6"}`},
+				// },
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "mail.", "A", "bind-import"),
+				// 	values: []interface{}{`{"data":"192.168.254.4"}`},
+				// },
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "ns1.", "A", "bind-import"),
+				// 	values: []interface{}{`{"data":"192.168.254.2"}`},
+				// },
+				// {
+				// 	key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "www.", "A", "bind-import"),
+				// 	values: []interface{}{`{"data":"192.168.254.7"}`},
+				// },
+			},
+		},
+		{
+			file: "./fixtures/example-all.zone",
+			kv: []kv{
+				{
+					key: ri.KV().Key(types.KeyRole, types.KeyZones, "example.com."),
+					values: []interface{}{
+						dns.Zone{DefaultTTL: 1814400, Authoritative: true, HandlerConfigs: []map[string]interface{}{
+							{
+								"type": "etcd",
+							},
+						}},
+					},
+				},
+				{
+					key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "mail.", "A", "bind-import"),
+					values: []interface{}{`{"data":"192.168.254.4"}`},
+				},
+				{
+					key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "mail.", "AAAA", "bind-import"),
+					values: []interface{}{`{"data":"2001:db8:3333:4444:5555:6666:7777:8888"}`},
+				},
+				{
+					key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", types.DNSRootRecord, "TXT", "bind-import"),
+					values: []interface{}{`{"data":"foo"}`},
+				},
+				{
+					key:    ri.KV().Key(types.KeyRole, types.KeyZones, "example.com.", "_sip._udp.", "SRV", "bind-import"),
+					values: []interface{}{`{"data":"fs1.example.com.","srvPort":5060,"srvPriority":10,"srvWeight":100}`},
+				},
+			},
+		},
 	}
 
 	api, stop := api.APIClient(rootInst)
@@ -76,6 +147,11 @@ func TestBindImport(t *testing.T) {
 
 	for _, file := range cases {
 		t.Run(file.file, func(t *testing.T) {
+			ri.KV().Delete(ctx, ri.KV().Key(
+				types.KeyRole,
+				types.KeyZones,
+			).Prefix(true).String(), clientv3.WithPrefix())
+
 			c, err := bind.New(api, file.file)
 			assert.NoError(t, err)
 			assert.NoError(t, c.Run(ctx))
