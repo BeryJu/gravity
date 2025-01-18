8be33fd1033a7ca30afe50a9a5b89d2e74cdf1be
Jens Langhammer <jens.langhammer@beryju.org>
2025-01-18 23:46:17 +0100

    migrate discovery simple

--- pkg/roles/discovery/subnet_test.go
+++ pkg/roles/discovery/subnet_test.go
@@ -5,7 +5,11 @@ import (
 
 	"beryju.io/gravity/pkg/extconfig"
 	"beryju.io/gravity/pkg/instance"
+	"beryju.io/gravity/pkg/roles/dhcp"
+	dhcpTypes "beryju.io/gravity/pkg/roles/dhcp/types"
 	"beryju.io/gravity/pkg/roles/discovery"
+	"beryju.io/gravity/pkg/roles/dns"
+	dnsTypes "beryju.io/gravity/pkg/roles/dns/types"
 	"beryju.io/gravity/pkg/tests"
 	"github.com/stretchr/testify/assert"
 )
@@ -16,37 +20,86 @@ const (
 	DockerIPCoreDNS = "10.200.0.4"
 )
 
-func TestDiscoveryDocker(t *testing.T) {
+func TestDiscoveryConvert(t *testing.T) {
 	defer tests.Setup(t)()
 	if !tests.HasLocalDocker() {
+		t.Skip("Local docker required")
 		return
 	}
 	extconfig.Get().ListenOnlyMode = false
 	rootInst := instance.New()
 	ctx := tests.Context()
-	inst := rootInst.ForRole("discovery", ctx)
-	role := discovery.New(inst)
+	role := discovery.New(rootInst.ForRole("discovery", ctx))
 	assert.NotNil(t, role)
 	assert.Nil(t, role.Start(ctx, []byte(tests.MustJSON(discovery.RoleConfig{
 		Enabled: true,
 	}))))
 	defer role.Stop()
 
+	inst := rootInst.ForRole("test", ctx)
+
+	// Create DNS Zone to register host in
+	tests.PanicIfError(inst.KV().Put(
+		ctx,
+		inst.KV().Key(
+			dnsTypes.KeyRole,
+			dnsTypes.KeyZones,
+			"example.com.",
+		).String(),
+		tests.MustJSON(dns.Zone{
+			HandlerConfigs: []map[string]interface{}{
+				{
+					"type": "etcd",
+				},
+			},
+		}),
+	))
+
+	// Create DHCP Scope to register host in
+	tests.PanicIfError(inst.KV().Put(
+		ctx,
+		inst.KV().Key(
+			dhcpTypes.KeyRole,
+			dhcpTypes.KeyScopes,
+			"test",
+		).String(),
+		tests.MustJSON(dhcp.Scope{
+			SubnetCIDR: DockerNetworkCIDR,
+			TTL:        86400,
+			IPAM: map[string]string{
+				"type":        "internal",
+				"range_start": "10.200.0.1",
+				"range_end":   "10.200.0.250",
+			},
+		}),
+	))
+
+	// Start DNS & DHCP to register events
+	dnsr := dns.New(rootInst.ForRole("dns", ctx))
+	assert.NotNil(t, dnsr)
+	assert.Nil(t, dnsr.Start(ctx, []byte(tests.MustJSON(dns.RoleConfig{
+		Port: -1,
+	}))))
+	defer dnsr.Stop()
+
+	dhcpr := dhcp.New(rootInst.ForRole("dhcp", ctx))
+	assert.NotNil(t, dhcpr)
+	assert.Nil(t, dhcpr.Start(ctx, []byte(tests.MustJSON(dhcp.RoleConfig{
+		Port: -1,
+	}))))
+	defer dhcpr.Stop()
+
+	// Run discovery
 	sub := role.NewSubnet("docker-test")
 	sub.CIDR = DockerNetworkCIDR
 	sub.DNSResolver = DockerIPCoreDNS
 	devices := sub.RunDiscovery(ctx)
-	assert.Equal(t, "10.200.0.1", devices[0].IP)
 
-	assert.Equal(t, "etcd.t.gravity.beryju.io", devices[1].Hostname)
-	assert.Equal(t, "10.200.0.2", devices[1].IP)
-	assert.Equal(t, "", devices[1].MAC)
-
-	assert.Equal(t, "minio.t.gravity.beryju.io", devices[2].Hostname)
-	assert.Equal(t, "10.200.0.3", devices[2].IP)
-	assert.Equal(t, "", devices[2].MAC)
-
-	assert.Equal(t, "coredns.t.gravity.beryju.io", devices[3].Hostname)
-	assert.Equal(t, "10.200.0.4", devices[3].IP)
-	assert.Equal(t, "", devices[3].MAC)
+	err := role.APIDevicesApply().Interact(ctx, discovery.APIDevicesApplyInput{
+		Identifier: devices[0].Identifier,
+		To:         "dhcp",
+		DHCPScope:  "test",
+		DNSZone:    "example.com.",
+	}, &struct{}{})
+	assert.NoError(t, err)
 }
---
+++ tests/discovery_e2e_test.go
@@ -0,0 +1,63 @@
+package tests
+
+import (
+	"testing"
+
+	"beryju.io/gravity/tests/gravity"
+	dockernetwork "github.com/docker/docker/api/types/network"
+	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/network"
+)
+
+func TestDiscovery_Simple(t *testing.T) {
+	ctx := Context(t)
+
+	net, err := network.New(
+		ctx,
+		network.WithIPAM(&dockernetwork.IPAM{
+			Driver: "default",
+			Config: []dockernetwork.IPAMConfig{
+				{
+					Subnet: "10.100.0.0/29",
+				},
+			},
+		}),
+		network.WithAttachable(),
+	)
+	assert.NoError(t, err)
+	testcontainers.CleanupNetwork(t, net)
+
+	gr := gravity.New(t,
+		gravity.WithNet(net),
+		// Use Docker gateway as DNS server to lookup name of other containers
+		gravity.WithEnv("FALLBACK_DNS", "10.100.0.1:53"))
+
+	tester, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			FromDockerfile: testcontainers.FromDockerfile{
+				Context:    "../hack/e2e/",
+				Dockerfile: "dns.Dockerfile",
+				Repo:       "gravity-dns-client",
+				KeepImage:  true,
+			},
+			Hostname: "client",
+			Networks: []string{net.Name},
+		},
+		Started: true,
+	})
+	testcontainers.CleanupContainer(t, tester)
+	assert.NoError(t, err)
+
+	_, err = gr.APIClient().RolesDiscoveryApi.
+		DiscoverySubnetStart(ctx).
+		Identifier("instance-subnet-gravity-1").
+		Wait(true).
+		Execute()
+	assert.NoError(t, err)
+
+	d, _, err := gr.APIClient().RolesDiscoveryApi.DiscoveryGetDevices(ctx).Execute()
+	assert.NoError(t, err)
+
+	assert.Len(t, d.Devices, 3)
+}
