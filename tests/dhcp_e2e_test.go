d0db59b1393a8ba762d8073a455d48b4954f49a5
Jens Langhammer <jens.langhammer@beryju.org>
2025-01-19 00:50:39 +0100

    dhcp: test WOL

--- tests/dhcp_e2e_test.go
+++ tests/dhcp_e2e_test.go
@@ -2,7 +2,9 @@ package tests
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 
 	"beryju.io/gravity/api"
 	"beryju.io/gravity/tests/gravity"
@@ -43,6 +45,7 @@ func TestDHCP_Simple(t *testing.T) {
 			"range_end":   "10.100.0.200",
 			"range_start": "10.100.0.100",
 			"type":        "internal",
+			"should_ping": "true",
 		},
 		Options: []api.TypesDHCPOption{},
 	}).Scope("network-A").Execute()
@@ -265,4 +268,88 @@ func TestDHCP_Relay(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Len(t, sc.Leases, 1)
 	assert.Equal(t, "10.101.0.100", sc.Leases[0].Address)
+}
+
+func TestDHCP_WOL(t *testing.T) {
+	ctx := Context(t)
+
+	net, err := network.New(
+		ctx,
+		network.WithIPAM(&dockernetwork.IPAM{
+			Driver: "default",
+			Config: []dockernetwork.IPAMConfig{
+				{
+					Subnet: "10.100.0.0/24",
+				},
+			},
+		}),
+		network.WithAttachable(),
+	)
+	assert.NoError(t, err)
+	testcontainers.CleanupNetwork(t, net)
+
+	g := gravity.New(t, gravity.WithNet(net))
+
+	ac := g.APIClient()
+	// Create test network
+	_, err = ac.RolesDhcpApi.DhcpPutScopes(ctx).DhcpAPIScopesPutInput(api.DhcpAPIScopesPutInput{
+		SubnetCidr: "10.100.0.0/24",
+		Ttl:        86400,
+		Ipam: map[string]string{
+			"range_end":   "10.100.0.200",
+			"range_start": "10.100.0.100",
+			"type":        "internal",
+			"should_ping": "true",
+		},
+		Options: []api.TypesDHCPOption{},
+	}).Scope("network-A").Execute()
+	assert.NoError(t, err)
+
+	// DHCP tester
+	tester, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			FromDockerfile: testcontainers.FromDockerfile{
+				Context:    "../hack/e2e/",
+				Dockerfile: "dhcp-client.Dockerfile",
+				Repo:       "gravity-dhcp-client",
+				KeepImage:  true,
+			},
+			Networks: []string{net.Name},
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.CapAdd = strslice.StrSlice{"NET_ADMIN"}
+			},
+		},
+		Started: true,
+	})
+	testcontainers.CleanupContainer(t, tester)
+	assert.NoError(t, err)
+
+	_, body := ExecCommand(t, tester, []string{"dhclient", "-v"})
+	assert.Contains(t, string(body), "DHCPOFFER of")
+	assert.Contains(t, string(body), "DHCPREQUEST for")
+	assert.Contains(t, string(body), "DHCPACK of")
+	assert.Contains(t, string(body), "bound to")
+
+	// Check correct lease exists
+	sc, _, err := ac.RolesDhcpApi.DhcpGetLeases(ctx).Scope("network-A").Execute()
+	assert.NoError(t, err)
+	assert.Len(t, sc.Leases, 1)
+	assert.Equal(t, "10.100.0.100", sc.Leases[0].Address)
+
+	// WOL
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+	go func() {
+		defer wg.Done()
+		cmd := "tcpdump -c 1 -UlnXi any ether proto 0x0842 or udp port 9 2>/dev/null "
+		_, body = ExecCommand(t, tester, []string{"bash", "-c", cmd})
+		assert.NotEqual(t, "", body)
+	}()
+	time.Sleep(3 * time.Second)
+	go func() {
+		defer wg.Done()
+		_, err := ac.RolesDhcpApi.DhcpWolLeases(ctx).Scope("network-A").Identifier(sc.Leases[0].Identifier).Execute()
+		assert.NoError(t, err)
+	}()
+	wg.Wait()
 }
