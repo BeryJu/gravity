688a36b07f3174aa82f237a3ec5c79b74d6c1322
Jens L. <jens.langhammer@beryju.org>
2025-01-19 00:37:28 +0100

    tests: cover bench (#1453)
    * cover benchmarks
    
    * test hook
    
    * more tests

---
+++ pkg/instance/role_instance_hook_test.go
@@ -0,0 +1,37 @@
+package instance_test
+
+import (
+	"testing"
+
+	"beryju.io/gravity/pkg/instance"
+	"beryju.io/gravity/pkg/roles"
+	"beryju.io/gravity/pkg/tests"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHook(t *testing.T) {
+	defer tests.Setup(t)()
+	rootInst := instance.New()
+	ri := rootInst.ForRole("test", tests.Context())
+	v := ri.ExecuteHook(roles.HookOptions{
+		Source: `function test() {
+			gravity.log("test");
+			return true;
+		}`,
+		Method: "test",
+	})
+	assert.True(t, v.(bool))
+}
+
+func TestHook_ParseIP(t *testing.T) {
+	defer tests.Setup(t)()
+	rootInst := instance.New()
+	ri := rootInst.ForRole("test", tests.Context())
+	v := ri.ExecuteHook(roles.HookOptions{
+		Source: `function test() {
+			return net.parseIP("192.168.1.100", "v4");
+		}`,
+		Method: "test",
+	})
+	assert.Equal(t, []byte{0xc0, 0xa8, 0x1, 0x64}, v.([]byte))
+}
