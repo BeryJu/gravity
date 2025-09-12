9c4261612b10bbc1ad20227ba81ff2a31173098d
Jens Langhammer <jens.langhammer@beryju.org>
2025-09-12 12:53:04 -0500

    etcd: start implementing more robust cluster joining

--- tests/cluster_e2e_test.go
+++ tests/cluster_e2e_test.go
@@ -31,29 +31,29 @@ func TestCluster_Join(t *testing.T) {
 		gravity.WithNet(net),
 	)
 
-	// // Create 3rd gravity node
-	// gravity3, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-	// 	ContainerRequest: testcontainers.ContainerRequest{
-	// 		Image:        "gravity:e2e-test",
-	// 		ExposedPorts: []string{"8008", "8009"},
-	// 		WaitingFor:   wait.ForHTTP("/healthz/ready").WithPort("8009"),
-	// 		Hostname:     "gravity-3",
-	// 		Networks:     []string{net.Name},
-	// 		Env: map[string]string{
-	// 			"LOG_LEVEL":         "debug",
-	// 			"ETCD_JOIN_CLUSTER": fmt.Sprintf("%s,http://gravity-1:8008", GravityToken),
-	// 			"GOCOVERDIR":        "/coverage",
-	// 		},
-	// 		HostConfigModifier: func(hostConfig *container.HostConfig) {
-	// 			hostConfig.Binds = []string{
-	// 				fmt.Sprintf("%s:/coverage", filepath.Join(cwd, "/coverage")),
-	// 			}
-	// 		},
-	// 	},
-	// 	Started: true,
-	// })
-	// testcontainers.CleanupContainer(t, gravity3)
-	// assert.NoError(t, err)
+	// Create 3rd gravity node
+	gravity3, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: testcontainers.ContainerRequest{
+			Image:        "gravity:e2e-test",
+			ExposedPorts: []string{"8008", "8009"},
+			WaitingFor:   wait.ForHTTP("/healthz/ready").WithPort("8009"),
+			Hostname:     "gravity-3",
+			Networks:     []string{net.Name},
+			Env: map[string]string{
+				"LOG_LEVEL":         "debug",
+				"ETCD_JOIN_CLUSTER": fmt.Sprintf("%s,http://gravity-1:8008", gravity.Token()),
+				"GOCOVERDIR":        "/coverage",
+			},
+			HostConfigModifier: func(hostConfig *container.HostConfig) {
+				hostConfig.Binds = []string{
+					fmt.Sprintf("%s:/coverage", filepath.Join(cwd, "/coverage")),
+				}
+			},
+		},
+		Started: true,
+	})
+	testcontainers.CleanupContainer(t, gravity3)
+	assert.NoError(t, err)
 
 	// Check that all nodes are in the cluster
 	ac := gr.APIClient()
