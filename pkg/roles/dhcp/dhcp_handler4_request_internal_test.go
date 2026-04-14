c076c14c5a2a6a419cbce2b38dd6ef918b6406c7
Roman Lumetsberger <roman.lumetsberger@outlook.com>
2026-04-14 21:15:21 +0200

    Add more tests to reach coverage goal

--- pkg/roles/dhcp/dhcp_handler4_request_internal_test.go
+++ pkg/roles/dhcp/dhcp_handler4_request_internal_test.go
@@ -2,6 +2,7 @@ package dhcp
 
 import (
 	"context"
+	"net"
 	"testing"
 	"time"
 
@@ -12,7 +13,7 @@ import (
 	"go.etcd.io/etcd/api/v3/mvccpb"
 )
 
-func TestDHCPDiscover_ReusesExistingLeaseWithoutDowngradingTTL(t *testing.T) {
+func TestDHCPRequest_ReusesStoredLeaseAndReassignsScopeOnWatcherMiss(t *testing.T) {
 	ctx := setupDHCPInternalTest(t)
 	inst := newDHCPTestInstance(ctx)
 	role := New(inst)
@@ -32,6 +33,23 @@ func TestDHCPDiscover_ReusesExistingLeaseWithoutDowngradingTTL(t *testing.T) {
 				"type":        "internal",
 				"range_start": "10.100.0.100",
 				"range_end":   "10.100.0.250",
+			},
+		}),
+	))
+	panicIfError(inst.KV().Put(
+		ctx,
+		inst.KV().Key(
+			types.KeyRole,
+			types.KeyScopes,
+			"test2",
+		).String(),
+		mustJSON(Scope{
+			SubnetCIDR: "10.200.0.0/24",
+			TTL:        86400,
+			IPAM: map[string]string{
+				"type":        "internal",
+				"range_start": "10.200.0.100",
+				"range_end":   "10.200.0.250",
 			},
 		}),
 	))
@@ -69,22 +87,34 @@ func TestDHCPDiscover_ReusesExistingLeaseWithoutDowngradingTTL(t *testing.T) {
 	)
 
 	req := &dhcpv4.DHCPv4{
-		OpCode:       dhcpv4.OpcodeBootRequest,
-		ClientHWAddr: []byte{0xb2, 0xb7, 0x86, 0x2c, 0xd3, 0xfa},
+		OpCode:        dhcpv4.OpcodeBootRequest,
+		GatewayIPAddr: net.ParseIP("10.200.0.1"),
+		ClientHWAddr:  []byte{0xb2, 0xb7, 0x86, 0x2c, 0xd3, 0xfa},
 	}
-	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeDiscover))
+	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeRequest))
 
 	req4 := role.NewRequest4(req)
-	res := role.HandleDHCPDiscover4(req4)
+	res := role.HandleDHCPRequest4(req4)
 	assert.NotNil(t, res)
-	assert.Equal(t, lease.Address, res.YourIPAddr.String())
+	assert.Equal(t, "10.200.0.100", res.YourIPAddr.String())
 
-	stored := role.FindLeaseInStore(req4)
-	assert.NotNil(t, stored)
-	assert.Greater(t, stored.Expiry, time.Now().Add(5*time.Minute).Unix())
+	storedKey := inst.KV().Key(
+		types.KeyRole,
+		types.KeyLeases,
+		lease.Identifier,
+	)
+	storedResp, err := inst.KV().Get(ctx, storedKey.String())
+	panicIfError(err)
+	assert.Len(t, storedResp.Kvs, 1)
+
+	stored, err := role.leaseFromKV(storedResp.Kvs[0])
+	panicIfError(err)
+	assert.Equal(t, "test2", stored.ScopeKey)
+	assert.Equal(t, "10.200.0.100", stored.Address)
+	assert.Greater(t, stored.Expiry, time.Now().Add(time.Hour).Unix())
 }
 
-func TestDHCPDiscover_ReturnsNilWhenNoScopeMatches(t *testing.T) {
+func TestDHCPRequest_ReturnsNilWhenNoScopeMatches(t *testing.T) {
 	ctx := setupDHCPInternalTest(t)
 	inst := newDHCPTestInstance(ctx)
 	role := New(inst)
@@ -96,13 +126,13 @@ func TestDHCPDiscover_ReturnsNilWhenNoScopeMatches(t *testing.T) {
 		OpCode:       dhcpv4.OpcodeBootRequest,
 		ClientHWAddr: []byte{0xb2, 0xb7, 0x86, 0x2c, 0xd3, 0xfa},
 	}
-	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeDiscover))
+	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeRequest))
 
 	req4 := role.NewRequest4(req)
-	assert.Nil(t, role.HandleDHCPDiscover4(req4))
+	assert.Nil(t, role.HandleDHCPRequest4(req4))
 }
 
-func TestDHCPDiscover_ReturnsNilWhenCreateLeaseFails(t *testing.T) {
+func TestDHCPRequest_ReturnsNilWhenCreateLeaseFails(t *testing.T) {
 	ctx := setupDHCPInternalTest(t)
 	inst := newDHCPTestInstance(ctx)
 	role := New(inst)
@@ -126,83 +156,19 @@ func TestDHCPDiscover_ReturnsNilWhenCreateLeaseFails(t *testing.T) {
 		}),
 	))
 
-	panicIfError(role.Start(ctx, []byte(mustJSON(RoleConfig{
-		Port:                  0,
-		LeaseNegotiateTimeout: 30,
-	}))))
+	panicIfError(role.Start(ctx, []byte(mustJSON(RoleConfig{Port: 0}))))
 	defer role.Stop()
 
 	req := &dhcpv4.DHCPv4{
 		OpCode:       dhcpv4.OpcodeBootRequest,
 		ClientHWAddr: []byte{0xb2, 0xb7, 0x86, 0x2c, 0xd3, 0xfa},
 	}
-	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeDiscover))
+	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeRequest))
 
 	req4 := role.NewRequest4(req)
 	cancelledCtx, cancel := context.WithCancel(req4.Context)
 	cancel()
 	req4.Context = cancelledCtx
 
-	assert.Nil(t, role.HandleDHCPDiscover4(req4))
-}
-
-func TestDHCPDiscover_ReturnsOfferWhenExistingLeaseRefreshFails(t *testing.T) {
-	ctx := setupDHCPInternalTest(t)
-	inst := newDHCPTestInstance(ctx)
-	role := New(inst)
-
-	panicIfError(inst.KV().Put(
-		ctx,
-		inst.KV().Key(
-			types.KeyRole,
-			types.KeyScopes,
-			"test",
-		).String(),
-		mustJSON(Scope{
-			SubnetCIDR: "10.100.0.0/24",
-			Default:    true,
-			TTL:        86400,
-			IPAM: map[string]string{
-				"type":        "internal",
-				"range_start": "10.100.0.100",
-				"range_end":   "10.100.0.250",
-			},
-		}),
-	))
-
-	panicIfError(role.Start(ctx, []byte(mustJSON(RoleConfig{
-		Port:                  0,
-		LeaseNegotiateTimeout: 30,
-	}))))
-	defer role.Stop()
-
-	scope, ok := role.scopes.GetPrefix("test")
-	assert.True(t, ok)
-	assert.NotNil(t, scope)
-
-	lease := role.NewLease("b2:b7:86:2c:d3:fa")
-	lease.scope = scope
-	lease.ScopeKey = scope.Name
-	lease.Address = "10.100.0.100"
-	panicIfError(lease.Put(ctx, 3600))
-
-	assert.Eventually(t, func() bool {
-		match, ok := role.leases.GetPrefix(lease.Identifier)
-		return ok && match != nil && match.Address == lease.Address
-	}, time.Second, 10*time.Millisecond)
-
-	req := &dhcpv4.DHCPv4{
-		OpCode:       dhcpv4.OpcodeBootRequest,
-		ClientHWAddr: []byte{0xb2, 0xb7, 0x86, 0x2c, 0xd3, 0xfa},
-	}
-	req.UpdateOption(dhcpv4.OptMessageType(dhcpv4.MessageTypeDiscover))
-
-	req4 := role.NewRequest4(req)
-	cancelledCtx, cancel := context.WithCancel(req4.Context)
-	cancel()
-	req4.Context = cancelledCtx
-
-	res := role.HandleDHCPDiscover4(req4)
-	assert.NotNil(t, res)
-	assert.Equal(t, lease.Address, res.YourIPAddr.String())
+	assert.Nil(t, role.HandleDHCPRequest4(req4))
 }
